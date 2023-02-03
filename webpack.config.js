f4b06aa6bfebc129c02340fcbb7baa4d2e9fc81d
Marina <marina.zilkina@gmail.com>
2023-02-04 01:14:43 +0200

    entry and output points; commented version to process;

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js'
+    },
+    // //entry point
+    // entry: path.join(__dirname, 'src', 'index.html'),
+    //     // output point
+    // output: {
+    //     path: path.join(__dirname, 'dist'),
+    //         /* [contenthash] - шаблон, в который подставляется хеш, сгенерированный на основе содержимого файла.
+    //         Хеширование меняет имя после каждого изменения содержимого файла. Это решает проблему версионирования.
+    //         Если хеш изменился, то браузер берет не старый файл из кеша, а загружает измененный с сервера.
+    //         Можно сократить 40 знаков хеша до нужного количества символов следующим образом: [contenthash:8].
+    //         Хеш будет длиной 8 символов. */
+    //     filename: 'index.[contenthash].js',
+    // },
     plugins: [
         // for plugin we create a new instance with 2 args:
         new HtmlWebpackPlugin({
